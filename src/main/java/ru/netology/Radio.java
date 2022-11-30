cfdab76bd12a1035ba48400018408b8fc7c856de
Vlad <Crash-vd@yandex.ru>
2022-11-30 13:06:35 +0300

    ОБНОВЛЕНИЕ РАДИО 2.0 (Задание 11) - Можно задавать количество радиостанций; - Новые парметры регулировки громкости звука; - Добавление механики конструктора в код; - Обновление кода и тестов.

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -2,66 +2,72 @@ package ru.netology;
 
 public class Radio {
     private int currentStation;
+    private int minStation = 0;
+    private int maxStation = 9;
+    private int radioStation = 10;
+    private int currentVolume;
+    private int minVolume = 0;
+    private int maxVolume = 100;
 
+    public Radio() {
+
+    }
+
+    public Radio(int radioStation) {
+        this.radioStation = radioStation;
+        this.maxStation = radioStation - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation || currentStation > radioStation) {
             return;
         }
-        if (newCurrentStation > 9) {
-            return;
-        }
-        currentStation = newCurrentStation;
-
+        this.currentStation = currentStation;
     }
 
     public void nextStation() {
-        if (9 > currentStation) {
-            setCurrentStation(currentStation + 1);
+        if (maxStation <= currentStation) {
+            setCurrentStation(minStation);
         } else {
 
-            setCurrentStation(0);
+            setCurrentStation(currentStation + 1);
+            ;
         }
     }
 
     public void prevStation() {
-        if (0 < currentStation) {
-            setCurrentStation(currentStation - 1);
+        if (minStation >= currentStation) {
+            setCurrentStation(maxStation);
         } else {
-            setCurrentStation(9);
+            setCurrentStation(currentStation - 1);
         }
     }
-
-    private int currentVolume;
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0) {
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
             return;
         }
-        if (newCurrentVolume > 10) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
+        this.currentVolume = currentVolume;
 
     }
 
     public void increaseVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
         }
 
     }
 
     public void decreaseVolume() {
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             currentVolume = currentVolume - 1;
         }
 
